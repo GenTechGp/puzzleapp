dbc788fa0ebdbfa8946dd5f9d350303419acd62a
Sasha Jenner <me@sjenner.com>
2024-09-18 10:59:50 +1000

    specify port and deps

--- run.R
+++ run.R
@@ -1,9 +1,20 @@
 #!/usr/bin/env Rscript
 # load an RData file and run the app
-# usage: ./run.R data.RData
+# optionally specify a localhost port
+# usage: ./run.R <RData> [port]
 
 library(shiny)
 
-args <- commandArgs(trailingOnly=TRUE)
+args <- commandArgs(trailingOnly = TRUE)
+
+if (length(args) == 2) {
+	port = as.integer(args[2])
+	if (is.na(port))
+		stop(sprintf("invalid port: '%s'", args[2]))
+	options(shiny.port = port)
+} else if (length(args) != 1) {
+	stop("usage: ./run.R <RData> [port]")
+}
+
 load(args[1])
 runApp()
