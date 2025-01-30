0d9f1b3b7b10218f455b02f0e22e5001795cf1d5
Andre Reis <a.martinsreis@garvan.org.au>
2025-01-30 13:10:16 +1100

    tidying up

--- app.R
+++ app.R
@@ -39,7 +39,7 @@ ui <- fluidPage(
     tabPanel("Home", homeUI("tab0")),
     tabPanel("Filters", selectFiltersUI("tab1", panel_app_genes)),
     tabUI("tab2", "Variants", outdir, TRUE, vtabvars, vtabsel),
-    igvUI("tab3", "IGV", pedigree_data$kinship),
+    igvUI("tab3", "IGV"),
     tabUI("tab4", "PanelApp", outdir, FALSE, names(panel_app), panel_app_vars),
     tabUI("tab5", "Phenotype", outdir, FALSE, names(phenotype_data), names(phenotype_data)),
     qcPlotsUI("tab6", "QC Plots")
