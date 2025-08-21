0dd9baf5800f85031997c7c014f68937c8a2de9f
Hiruna Samarakoon <hiruna72@gmail.com>
2025-08-21 17:02:27 +1000

    rename files

---
+++ R/_PACKAGE.R
@@ -0,0 +1,7 @@
+# R/_PACKAGE.R
+
+#' PuzzleApp
+#'
+#' A Shiny App to visualise and analyse variants.
+#'
+"_PACKAGE"
--- R/puzzleapp-package.R
+++
@@ -1,7 +0,0 @@
-#' puzzleapp: A Shiny App to visualise and analyse variants
-#'
-#' This package contains a Shiny application bundled as an R package.
-#'
-#' @docType package
-#' @name puzzleapp
-NULL
