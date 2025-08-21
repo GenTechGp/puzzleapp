5057e460b5996f8e3f3c720f3949c26eefa9ad22
Hiruna Samarakoon <hiruna@unsw.edu.au>
2025-08-21 16:05:44 +1000

    cleanup

--- R/extra.R
+++
@@ -1,4 +0,0 @@
-  # Requires DataTables version >= 1.10
-  #renderDT(datatable(tab, list(searching = FALSE, paging = FALSE, info = FALSE,
-  #                   ordering = FALSE), "compact", rownames = FALSE,
-  #                   escape = FALSE, selection = "none"))
